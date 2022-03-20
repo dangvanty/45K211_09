351a1622d9ea8dd7fcac5a13a0a8e7fb807cacf1
Nguyễn Thị Nữ <nunguyenthi2001@gmail.com>
2022-03-20 19:32:59 +0700

    Backend User&Password Authentication

---
+++ dong_dung_web/backend/models/userModel.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function () {
+    // Generating Token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+  
+    // Hashing and adding resetPasswordToken to userSchema
+    this.resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+  
+    this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+  
+    return resetToken;
+  };
+  
+  module.exports = mongoose.model("User", userSchema);
