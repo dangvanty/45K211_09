9afdf633a145ca6af46aec02d965cf7bcc938d1a
Dang Van Ty <dangvantydh1@gmail.com>
2022-03-23 15:00:45 +0700

    connect mongodb and product model

--- dong_dung_web/backend/server.js
+++ dong_dung_web/backend/server.js
@@ -1,9 +1,13 @@
 const app = require("./app");
 const dotenv=require("dotenv");
-
+const connectDatabase =require('./config/database');
 
 //config
-dotenv.config({path:"backend/config/config.env"})
+dotenv.config({path:"backend/config/config.env"});
+
+//connect to database:
+connectDatabase()
+
 
 app.listen(process.env.PORT,()=>{
  console.log(`Server is working successfully on http://localhost:${process.env.PORT}`)
