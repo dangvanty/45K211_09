0ed31fc54d1a9c420efac0ee4e322f6bff76d25d
Dang Van Ty <dangvantydh1@gmail.com>
2022-03-23 17:50:35 +0700

    CRUD cho product

--- dong_dung_web/backend/controller/productController.js
+++ dong_dung_web/backend/controller/productController.js
@@ -1,4 +1,74 @@
-exports.getAllProducts = (rep,res)=>{
-    res.status(200).json({message:"Route is working fine"})
+const Product =require("../models/productModel");
+
+//Created Product--Admin
+exports.createProduct= async(req,res,next)=> {
+    const product =await Product.create(req.body);
+    
+}
+
+// get all product 
+exports.getAllProducts = async (req,res)=>{
+    const products = await Product.find()
+    res.status(200).json({
+        success:true,
+        products
+    })
+}
+
+//get product details
+exports.getProductDetails =async(req,res,next)=>{
+    const product = await Product.findById(req.params.id)
+    
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    
+    res.status(200).json({
+        success:true,
+        product
+    })
+
+}
+
+//update product -- admin
+exports.updateProduct = async(req,res,next)=>{
+    let product = await Product.findById(req.params.id)
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true, 
+        useFindAndModify:false
+    })
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+//delete product
+
+exports.deleteProduct= async (req,res,next)=>{
+    const product = await Product.findById(req.params.id)
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"Product Deleted successfully",
+        product
+    })
 }
 
--- dong_dung_web/backend/routes/productRoute.js
+++ dong_dung_web/backend/routes/productRoute.js
@@ -1,7 +1,9 @@
 const express=require("express");
-const { getAllProducts } = require("../controller/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails} = require("../controller/productController");
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts)
+router.route("/products").get(getAllProducts);
+router.route("/products/new").post(createProduct);
+router.route("/products/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
 module.exports=router
