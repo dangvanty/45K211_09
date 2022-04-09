6d15c993d5f90efae70104923b90542352d3db19
Dang Van Ty <dangvantydh1@gmail.com>
2022-04-09 11:50:28 +0700

    API order

--- dong_dung_web/backend/controller/orderController.js
+++ dong_dung_web/backend/controller/orderController.js
@@ -33,3 +33,103 @@ exports.CreateNewOrder=catchAsyncErrors(async(req,res,next)=>{
 });
 
 
+// get Single Oder
+
+exports.getSingleOrder = catchAsyncErrors(async (req,res,next)=>{
+    const order =await Order.findById(req.params.id).populate(
+        "user",
+        "name email"
+    );
+
+    if(!order){
+        return next(new ErrorHander("Order không được tìm thấy tại Id này",404));
+    }
+
+    res.status(200).json({
+        success:true,
+        order,
+    });
+});
+
+//get log in user Order
+
+exports.myOrders=catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user._id });
+  
+    res.status(200).json({
+      success: true,
+      orders,
+    });
+  });
+
+//get all order from admin
+
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find();
+  
+    let totalAmount = 0;
+  
+    orders.forEach((order) => {
+      totalAmount += order.totalPrice;
+    });
+  
+    res.status(200).json({
+      success: true,
+      totalAmount,
+      orders,
+    });
+  });
+  
+  // update order status -admin
+  exports.updateOrder = catchAsyncErrors(async(req,res,next)=>{
+      const order =await Order.findById(req.params.id);
+
+      if (!order) {
+        return next(new ErrorHander("Order không được tìm thấy tại Id này ", 404));
+      }
+
+      if (order.orderStatus === "Delivered") {
+        return next(new ErrorHander("Đơn hàng đang được Delivered", 400));
+      }
+
+      if(req.body.status==="Shipped"){
+        order.orderItems.forEach(async (o) => {
+            await updateStock(o.product, o.quantity);
+
+        });
+      }
+      order.orderStatus=req.body.status;
+
+      if (req.body.status === "Delivered") {
+        order.deliveredAt = Date.now();
+      }
+    
+      await order.save({ validateBeforeSave: false });
+      res.status(200).json({
+        success: true,
+      });
+  })
+
+  async function updateStock(id, quantity) {
+    const product = await Product.findById(id);
+  
+    product.Stock -= quantity;
+  
+    await product.save({ validateBeforeSave: false });
+  }
+  
+  //delete order ---admin
+
+  exports.deleteOrder=catchAsyncErrors (async(req,res,next)=>{
+      const order=await Order.findById(req.params.id);
+
+      if (!order) {
+        return next(new ErrorHander("Order không được tìm thấy tại Id này ", 404));
+      }
+
+      await order.remove();
+
+      res.status(200).json({
+        success: true,
+      });      
+  })
--- dong_dung_web/backend/routes/orderRoute.js
+++ dong_dung_web/backend/routes/orderRoute.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const { CreateNewOrder } = require("../controller/orderController");
+const { CreateNewOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require("../controller/orderController");
 
 const router =express.Router();
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route("/order/new").post(isAuthenticatedUser,CreateNewOrder)
+
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+
+router.route("/orders/me").get(isAuthenticatedUser, myOrders)
+
+router
+    .route("/admin/orders")
+    .get(isAuthenticatedUser, authorizeRoles("admin"),getAllOrders);
+
+router
+    .route("/admin/order/:id")
+    .put(isAuthenticatedUser, authorizeRoles("admin"),updateOrder)
+    .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteOrder);
 
 module.exports=router;
