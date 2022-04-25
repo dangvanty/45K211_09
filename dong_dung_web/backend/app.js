0ded43e5a73cd3102a84f2b6db43b90cbe20a884
Dang Van Ty <dangvantydh1@gmail.com>
2022-04-25 07:55:27 +0700

    deploy prepare

--- dong_dung_web/backend/app.js
+++ dong_dung_web/backend/app.js
@@ -4,8 +4,13 @@ const bodyParser = require("body-parser");
 const app = express();
 const routes = require('./routes');
 const fileUpload = require("express-fileupload");
-
+const path =require("path")
 const errorMiddleware= require('./middleware/error')
+
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+require("dotenv").config({ path: "backend/config/config.env" });
+}
 // support post API
 app.use(express.json({ limit: "50mb" }));
 app.use(express.json());
@@ -15,6 +20,11 @@ app.use(fileUpload());
 // call router
 routes(app);
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 //middleware for error
 
 app.use(errorMiddleware);
--- dong_dung_web/backend/server.js
+++ dong_dung_web/backend/server.js
@@ -1,11 +1,11 @@
 const app = require("./app");
-const dotenv=require("dotenv");
 const cloudinary = require('cloudinary');
 const connectDatabase =require('./config/database');
 
 //config
-dotenv.config({path:"backend/config/config.env"});
-
+if (process.env.NODE_ENV !== "PRODUCTION") {
+require("dotenv").config({ path: "backend/config/config.env" });
+}
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`);
